fad71d8c6d7ae71bd738d38fa984e6b0aaedd7cb
Fernando del Campo Vergara <fdelcampo@gmail.com>
2022-05-03 16:12:23 -0400

    changes about the create log container using the both component of edit and view log of OLE

--- love/src/components/UIF/ComponentIndex.jsx
+++ love/src/components/UIF/ComponentIndex.jsx
@@ -198,6 +198,16 @@ export const observatoryIndex = {
       },
     },
   },
+  CreateOLETabs: {
+    component: require('../OLE/CreateOLETabs.container').default,
+    schema: {
+      ...require('../OLE/CreateOLETabs.container').schema,
+      props: {
+        ...defaultSchemaProps,
+        ...require('../OLE/CreateOLETabs.container').schema.props,
+      },
+    },
+  },
 };
 
 export const auxtelIndex = {
